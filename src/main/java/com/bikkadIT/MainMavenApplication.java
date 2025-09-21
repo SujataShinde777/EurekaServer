54f29fc468773b5210bce866ca6fc76ca032cab9
SujataShinde777 <sujushinde777@gmail.com>
2025-09-21 16:09:38 +0530

    this is maven project

---
+++ src/main/java/com/bikkadIT/MainMavenApplication.java
@@ -0,0 +1,14 @@
+package com.bikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainMavenApplication.class, args);
+//old task
+	}
+
+}
---
+++ src/main/java/com/bikkadIT/add/Addition.java
@@ -0,0 +1,10 @@
+package com.bikkadIT.add;
+
+public class Addition {
+
+	
+	public int add(int a, int b) {
+		
+		return a + b;
+	}
+}
